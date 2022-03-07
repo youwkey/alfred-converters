981285f2ca2832acebb060b5756c06bdf4a8e4a0
youwkey <youwkey@gmail.com>
2022-03-07 21:30:27 +0900

    chore: create base files

---
+++ cmd/conv/main.go
@@ -0,0 +1,11 @@
+// Copyright 2022 youwkey. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("implements me")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/youwkey/alfred-mutual-converter
+
+go 1.17
